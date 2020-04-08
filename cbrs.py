b03a029787f15aa52f4bb675128163c33271e60b
yammadev <yefferson.marin@gmail.com>
2020-04-08 00:47:58 -0500

    Juypiter notebook finished + code cleaning

--- cbrs.py
+++ cbrs.py
@@ -17,7 +17,7 @@ def main():
     # print(f'\n{cases}')
 
     # Select columns from library to use as base cases, except solutions
-    base = library.iloc[:, range(library.shape[1] - 1)]      # Exclude last column
+    base = library.iloc[:, range(library.shape[1] - 1)]      # Exclude last column (solution)
 
     # Print
     # print('\n> Base')
@@ -36,60 +36,60 @@ def main():
     # Print
     print('\n> Calculating\n')
 
-    # [3.1] Move through all problem cases
+    # Move through all problem cases
     for i in range(problems.shape[0]):
         # Print
         # print(f'\n{base} for problem {i}')
 
-        # Get inverse covariance matrix for the base cases
+        # [3.1] Get inverse covariance matrix for the base cases
         covariance_matrix = base.cov()                                      # Covariance
         inverse_covariance_matrix = np.linalg.pinv(covariance_matrix)       # Inverse
 
-        # Get case row to evaluate
+        # [3.2] Get case row to evaluate
         case_row = problems.loc[i, :]
 
         # Empty distances array to store mahalanobis distances obtained comparing each library cases
         distances = np.zeros(base.shape[0])
 
-        # [3.2] For each base cases rows
+        # [3.3] For each base cases rows
         for j in range(base.shape[0]):
             # Get base case row
             base_row = base.loc[j, :]
 
-            # [3.3] Calculate mahalanobis distance between case row and base cases, and store it
+            # [3.4] Calculate mahalanobis distance between case row and base cases, and store it
             distances[j] = distance.mahalanobis(case_row, base_row, inverse_covariance_matrix)
 
-        # [3.4] Returns the index (row) of the minimum value in distances calculated
+        # [3.5] Returns the index (row) of the minimum value in distances calculated
         min_distance_row = np.argmin(distances)
 
-        # [3.5] Get solution based on index of found minimun distance, and append it to main library
+        # [4] Get solution based on index of found minimum distance, and append it to main library
         # From cases, append library 'similar' solution
         case = np.append(cases.iloc[i, :], library.iloc[min_distance_row, -1])
 
         # Print
         print(f'> For case/problem {i}: {cases.iloc[i, :].to_numpy()}, solution is {case[-1]}')
 
-        # [4] Store
+        # [5] Store
         # Get as operable pandas Series
         case = pd.Series(case, index = library.columns)         # Case with Solution
         library = library.append(case, ignore_index = True)     # Append to library
 
-        # Save output as file
+        # Save 'covariance heat map (biased)' output as file
         sn.heatmap(np.cov(base, bias = True), annot = True, fmt = 'g')
         plt.gcf().set_size_inches(12, 6)
-        plt.title(f'Covariance Heatmap #{i} \n Library cases stored {j} - Base to solve problem {i}')
-        plt.savefig(f'output/covariance_heatmap_{i}.png', bbox_inches='tight')
+        plt.title(f'Covariance Heat map #{i} \n Library cases stored {j} - Base to solve problem {i}')
+        plt.savefig(f'output/covariance_heat_map_{i}.png', bbox_inches='tight')
         plt.close()
 
-        # [5] Reuse
-        base = library.iloc[:, range(library.shape[1] - 1)]     # Exclude last column
+        # [6] Reuse
+        base = library.iloc[:, range(library.shape[1] - 1)]     # Exclude last column (solution)
         base = pd.get_dummies(base)                             # Get new one-hot encoded base
 
-    # [6] Output
+    # [7] Output
     print('\n> Output library')
     print(f'\n{library}')
 
-    # Save output as file
+    # Save 'library' output as file
     library.to_csv('output/library.csv', index = False)
 
 # Call
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Case-based reasoning (CBR) system
-A **Case-based reasoning** (CBR) is a paradigm of artificial intelligence and cognitive science that models the reasoning process as primarily memory based. Case-based reasoners systems solve new problems by retrieving stored ‘cases’ describing similar prior problem-solving episodes and adapting their solutions to fit new needs.
+A **Case-based reasoning** (CBR) is a paradigm of artificial intelligence and cognitive science that models the reasoning process as primarily memory based. Case-based reasoning systems solve new problems by retrieving stored ‘cases’ describing similar prior problem-solving episodes and adapting their solutions to fit new needs.
 
 Case-based reasoning has been [formalized](https://www.idi.ntnu.no/emner/tdt4171/papers/AamodtPlaza94.pdf) for purposes of computer reasoning as a four-step process:
 
@@ -11,7 +11,7 @@ Case-based reasoning has been [formalized](https://www.idi.ntnu.no/emner/tdt4171
 ### Author
 Yefferson Marín - ([@yammadev](https://github.com/yammadev))
 
-## Requeriments
+## Requirements
 * [Python 3.*](https://python.org)
 * [Pandas](https://pandas.pydata.org/)
 * [Numpy](https://numpy.org)
@@ -82,17 +82,17 @@ Initial library plus cases/problems solved. At [output/library.csv](output/libra
 |Rainy   | Hot       | High   | True | No |
 |Rainy   | Cool      | High   | True | No |
 
-### Heatmaps
-An image for each Covariance Heatmap, using library cases stored at each iteration base to solve an especific problem. At [output/heatmap_*.png](output)
+### Heat maps
+An image for each Covariance Heat map, using library cases stored (base) at each iteration to solve a specific problem. At [output/heat_map_x.png](output)
 
-![heatmap_0](output/covariance_heatmap_0.png)
-![heatmap_1](output/covariance_heatmap_1.png)
-![heatmap_2](output/covariance_heatmap_2.png)
-![heatmap_3](output/covariance_heatmap_3.png)
-![heatmap_4](output/covariance_heatmap_4.png)
-![heatmap_5](output/covariance_heatmap_5.png)
+![heatmap_0](output/covariance_heat_map_0.png)
+![heatmap_1](output/covariance_heat_map_1.png)
+![heatmap_2](output/covariance_heat_map_2.png)
+![heatmap_3](output/covariance_heat_map_3.png)
+![heatmap_4](output/covariance_heat_map_4.png)
+![heatmap_5](output/covariance_heat_map_5.png)
 
-## Devopment notes
+## Development notes
 ```sh
   # Install virtual environment
   $ python3 -m venv venv
@@ -133,12 +133,20 @@ All notable changes to this project are documented in this part of the file. The
 - **Fixed for** any bug fixes.
 - **Security** in case of vulnerabilities.
 
+### [2.2.0] - 2020-04-07
+#### Added
+- `Jupiter Notebook` finished.
+
+#### Modified
+- Spell checking
+- Code cleaning.
+
 ### [2.1.0] - 2020-04-07
 #### Added
 - Outputs and code cleaning.
 
 #### Modified
-- Main functionalities to a better optimizations.
+- Main functionalities to better optimizations.
 - `Readme` edited.
 
 ### [2.0.0] - 2020-04-07
